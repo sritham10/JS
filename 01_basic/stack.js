9027c3d60c01f6fbe1ca928971a7428687b21965
Sritham Choudhury <83761482+sritham10@users.noreply.github.com>
2024-07-12 08:48:26 +0000

    new file added

---
+++ 01_basic/stack.js
@@ -0,0 +1,19 @@
+//stack memeory(primitive) andd heap memory(non primitive)
+
+stack-copy milta hai
+heap=refernce milta hai original value mai change Hoga
+
+ex-
+
+
+let youtubename="srithamfromyt"
+let anothername =youtubename
+anothername="chaiaurcode"
+console.log(youtubename)
+console.log(anothername)
+
+let user={
+    email:"user@gmail.com"
+    upi:"user@ybl"
+}
+
--- 01_basic/summ.js
+++ 01_basic/summ.js
@@ -1,3 +1,31 @@
 //primitive
 
-// 7 types : String
+// 7 types : String,number ,boolean,null,defined,symbol
+
+//call by value-copy of data
+
+
+//call by refernce(non prmitive)
+//Array,objects,function
+
+// const score =100 
+//const scorevalue=100.3
+//const is loggedin = false
+//let useEmail
+
+const id=symbol('123')
+
+// basic od non-primitive
+
+const heross= ["shakiman","batman"];
+let myobj=
+{
+    name:"sritham",
+    age:22,
+}
+
+const myfunction=function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myfunction);
